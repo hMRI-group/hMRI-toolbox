c246cd374caf67040f2e1e684de4f8629937e8c9
ebalteau <e.balteau@ulg.ac.be>
2016-12-06 13:22:35 +0100

    Version of the toolbox:
    If fails using MATLAB-git and git to retrieve the current version of the
    Toolbox, searches for existing version.txt file.
    If none available, returns "Unknown version".

--- import_and_filters/hMRI_get_version.m
+++ import_and_filters/hMRI_get_version.m
@@ -2,6 +2,9 @@ function version = hMRI_get_version
 % To retrieve the SHA1, author, date and message of the last commit of the
 % current branch of the repository and return the information as a string.
 % This script MUST be located in the root directory of the repository.
+% If the Toolbox has been copied whitout version tracking, the version can
+% only be retrieved if a version.txt file is already present in the root
+% directory of the Toolbox.
 %
 % DEPENDENCIES
 % This script calls the git command using the MATLAB-git wrapper from
@@ -25,24 +28,44 @@ repos_dir = fileparts(mfilename('fullpath'));
 current_dir = pwd;
 % move to the repository
 cd(repos_dir);
-% execute the git commant
-git log -1 > version.txt
-% open the version.txt file
-version_fname = fullfile(repos_dir,'version.txt');
-fid = fopen(version_fname,'r');
 
+% initialise the output variable:
 version = [];
 
-if (fid~=-1)
-    % read file content
-    clin = fgets(fid);
-    while (clin~=-1)
-        version = [version clin];
-        clin = fgets(fid);
-    end
-    fclose(fid);
+try
+    % execute the git command to output the current version into text file:
+    git log -1 > version.txt
+catch MExc %#ok<*NASGU>
+    % warning('Either or both MATLAB-git and git are not available on this machine.');
+    fprintf(1,['\nWARNING:\nEither or both MATLAB-git and git are not available on this machine.\n'...
+        'The current version of the hMRI-Toolbox cannot be retrieved.\n'...
+        'See ''help hmri_get_version'' for details about dependencies.\n'...     
+        'Searching for an existing version.txt file...\n']);
+    fprintf(1,'\n%s\n', MExc.getReport);
+end
+
+% version.txt file should be the following:
+version_fname = fullfile(repos_dir,'version.txt');
+
+if ~exist(version_fname,'file')
+    fprintf(1,'File %s does not exist.\nhMRI-Toolbox version unknown.\n\n', version_fname);
+    version = 'Unknown version';
 else
-    warning('Cannot open file %s.', version_fname);
+    % try to open the version.txt file:
+    fid = fopen(version_fname,'r');
+    if (fid~=-1)
+        % read file content
+        clin = fgets(fid);
+        while (clin~=-1)
+            version = [version clin];
+            clin = fgets(fid);
+        end
+        fclose(fid);
+        % fprintf(1,'Toolbox version:\n%s\n', version);
+    else
+        fprintf(1,'Cannot open file %s.\nhMRI-Toolbox version unknown.\n\n', version_fname);
+        version = 'Unknown version';
+    end
 end
 
 % back to the current working directory

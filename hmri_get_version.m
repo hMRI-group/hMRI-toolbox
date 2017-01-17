3908483c9818926bcd61e69b0f0b538b00bb57b5
ebalteau <e.balteau@ulg.ac.be>
2017-01-17 15:39:43 +0100

    hmri_get_version
    - added scripts for automatic update of the version.txt file
    - modified hmri_get_version's help and comments accordingly
    - added hmri_get_version_readme.pdf as help file

--- hmri_get_version.m
+++ hmri_get_version.m
@@ -1,4 +1,4 @@
-function version = hMRI_get_version
+function version = hmri_get_version
 % To retrieve the SHA1, author, date and message of the last commit of the
 % current branch of the repository and return the information as a string.
 % This script MUST be located in the root directory of the repository.
@@ -6,16 +6,20 @@ function version = hMRI_get_version
 % only be retrieved if a version.txt file is already present in the root
 % directory of the Toolbox.
 %
-% DEPENDENCIES
+% DEPENDENCIES (NOT MANDATORY)
 % This script calls the git command using the MATLAB-git wrapper from
 % https://github.com/manur/MATLAB-git.git. The latter allows you to use
 % command line git instructions in Matlab (as long as Git is installed on
 % your computer!). Make sure that the git.m script is in the Matlab path to
-% execute this script.
+% execute this script. 
 %
 % COMMAND LINE EQUIVALENT IN GIT BASH 
 % (to output the information into version.txt)
 % § git log -1 > version.txt
+%
+% ALTERNATIVELY, see version/hmri_get_version_readme.pdf for alternative
+% way to update the version.txt file using post-commit and post-merge
+% scripts.
 %--------------------------------------------------------------------------
 % Written by Evelyne Balteau - May 2016
 % Cyclotron Research Centre, University of Liege
@@ -36,12 +40,11 @@ try
     % execute the git command to output the current version into text file:
     git log -1 > version.txt
 catch MExc %#ok<*NASGU>
-    % warning('Either or both MATLAB-git and git are not available on this machine.');
-    fprintf(1,['\nWARNING:\nEither or both MATLAB-git and git are not available on this machine.\n'...
-        'The current version of the hMRI-Toolbox cannot be retrieved.\n'...
-        'See ''help hmri_get_version'' for details about dependencies.\n'...     
-        'Searching for an existing version.txt file...\n']);
-    fprintf(1,'\n%s\n', MExc.getReport);
+    % fprintf(1,['\nWARNING:\nEither or both MATLAB-git and git are not available on this machine.\n'...
+    %    'The current version of the hMRI-Toolbox cannot be retrieved.\n'...
+    %    'See ''help hmri_get_version'' for details about dependencies.\n'...     
+    %    'Searching for an existing version.txt file...\n']);
+    % fprintf(1,'\n%s\n', MExc.getReport);
 end
 
 % version.txt file should be the following:

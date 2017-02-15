5f7c4b2f5bf4a4ffd8ce6ade2255f4b4b10b8f04
ebalteau <e.balteau@ulg.ac.be>
2017-02-15 10:32:44 +0100

    Fixed but in has_extended_header
    Must strip ",1" from the ".nii,1" file extension when present. Error at
    opening the file with fopen otherwise.

--- spm12/metadata/has_extended_header.m
+++ spm12/metadata/has_extended_header.m
@@ -1,4 +1,7 @@
 function isHdrExtended = has_extended_header(niifilenam)
+% Must strip the ',1' (at the end of the file extension '.nii,1') 
+% if the name has been collected using e.g. spm_select:
+niifilenam = spm_file(niifilenam,'number','');
 % Check whether a NIFTI file has extended header or not...
 fid = fopen(niifilenam,'r+');
 std_hdr_size = 348;

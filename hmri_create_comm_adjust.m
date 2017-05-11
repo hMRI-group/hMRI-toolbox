b7df11d20c429ac9808326f4e441b91b37adfc49
ebalteau <e.balteau@ulg.ac.be>
2017-05-11 12:40:41 +0200

    Use SPM progress bas in ACPC reorient
    Instead of Matlab progress bar, for uniformity and also avoiding appearance of
    unidentified window (too fast to be able to read the message).

--- hmri_create_comm_adjust.m
+++ hmri_create_comm_adjust.m
@@ -2,37 +2,37 @@ function [M,R] = hmri_create_comm_adjust(option,Ref,Other,Nits,doshear,Template)
 
 % This function is a version of comm_adjust.m customized for T1 like
 % images, i.e. MT images. Now it is necessary to define as input parameter
-% the Template to be used for the commisure adjustment. 
+% the Template to be used for the commisure adjustment.
 %
 %% Lester Melie-Garcia
-% LREN, CHUV. 
+% LREN, CHUV.
 % Lausanne, December 6th, 2016
 
 if option,
     switch nargin
-        case 1, Nits = 2; doshear = 0; 
+        case 1, Nits = 2; doshear = 0;
             %Modality = input('modality of image?: ','s');
             Ref = myselect(Inf,'image','select file to adust',sel,wd,'.*','1');
             Other = myselect(Inf,'image','select other files to adust','',pwd,'.*','1');
             if isempty(Other), filenames = Ref; else
-                filenames = unique(strvcat(Ref,Other),'rows');
+                filenames = unique(strvcat(Ref,Other),'rows'); %#ok<*FPARK>
             end
         case 2,
             %Modality = input('modality of image?: ','s');
             Other = myselect(Inf,'image','select other files to adust','',pwd,'.*','1');
-             if isempty(Other), filenames = Ref; else
+            if isempty(Other), filenames = Ref; else
                 filenames = unique(strvcat(Ref,Other),'rows');
             end
         case 3, Nits = 2; doshear = 0;
             Other = myselect(Inf,'image','select other files to adust','',pwd,'.*','1');
-             if isempty(Other), filenames = Ref; else
+            if isempty(Other), filenames = Ref; else
                 filenames = unique(strvcat(Ref,Other),'rows');
             end
-        case 4, Nits = 2; doshear = 0; 
+        case 4, Nits = 2; doshear = 0;
             filenames = unique(strvcat(Ref,Other),'rows');
-        case 5, doshear = 0; 
+        case 5, doshear = 0;
             filenames = unique(strvcat(Ref,Other),'rows');
-        case 6, doshear = logical(doshear); 
+        case 6, doshear = logical(doshear);
             filenames = unique(strvcat(Ref,Other),'rows');
         otherwise, error('too or few inputs!')
     end
@@ -40,7 +40,7 @@ else
     switch nargin
         case 2, Nits = 2; doshear = 0; filenames = Ref; %Modality = 'T1';
         case 3, Nits = 2; doshear = 0; filenames = Ref;
-        case 4, Nits = 2; doshear = 0; 
+        case 4, Nits = 2; doshear = 0;
             filenames = unique(strvcat(Ref,Other),'rows');
         case 5, doshear = 0;
             filenames = unique(strvcat(Ref,Other),'rows');
@@ -55,11 +55,11 @@ if ~exist('Template','var')
     Template = fullfile(spm('Dir'),'canonical','avg152T1.nii');
 end;
 flags = struct('WG'      ,[]    ,...
-               'WF'      ,[]    ,...
-               'sep'     ,8     ,...
-               'regtype' ,'mni' ,...
-               'globnorm',0);
-           
+    'WF'      ,[]    ,...
+    'sep'     ,8     ,...
+    'regtype' ,'mni' ,...
+    'globnorm',0);
+
 VRef = spm_smoothto8bit(spm_vol(Ref),8);
 VTmp = spm_smoothto8bit(spm_vol(Template),0);
 %VTmp = spm_smoothto8bit(spm_vol(Tmp),0);
@@ -71,16 +71,19 @@ for i = 2:Nits,
     flags.sep = sep(i);
     [M,scal] = spm_affreg(VTmp,VRef,flags,M,scal); %VTmp.mat\M*VRef.mat
 end
-if doshear, R = M; else, 
-    [A,B,C] = svd(M(1:3,1:3)); R = A*C';
+if doshear
+    R = M; 
+else
+    [A,B,C] = svd(M(1:3,1:3)); R = A*C'; %#ok<ASGLU>
     R(:,4) = R*(M(1:3,1:3)\M(1:3,4)); R(4,4) = 1;
 end
-warning off
-h = waitbar(0,'reorienting to AC-PC line...');
+
+spm_progress_bar('Init',size(filenames,1),'Reorient to AC-PC line','volumes completed');
 for i = 1:size(filenames,1),
     spm_get_space(deblank(filenames(i,:)),...
         R*spm_get_space(deblank(filenames(i,:))));
-    waitbar(i/size(filenames,1),h);
+    spm_progress_bar('Set',i);
 end
-close(h);
-warning on
+spm_progress_bar('Clear');
+
+end

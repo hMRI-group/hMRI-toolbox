f3e8563d7e073c641a088f8484aa0dd474570e8e
ebalteau <e.balteau@ulg.ac.be>
2017-09-25 16:39:29 +0200

    Autoreorient implementation tested...
    Does the job all right with the datasets used (including awkward initial
    orientation from elder patients scanned on the Allegra, as well as more
    standard datasets (Prisma)).
    
    The template must be choosen according to the contrast of the
    reference image (e.g. the avg152PD.nii canonical template is well suited
    for MTw and PDw echoes, while the avg152T1.nii canonical template is
    preferred for T1w echoes).

--- hmri_autoreorient.m
+++ hmri_autoreorient.m
@@ -6,8 +6,8 @@ function outfiles = hmri_autoreorient(ref, template, other)
 % image (or any other usual image modality) in the MNI space, i.e. mainly
 % set the AC location and correct for head rotation, in order to further
 % proceed with the segmentation/normalisation of the image.
-% A set of other images can be reoriented along the 1st one. They should be
-% specified as "Other Images".
+% A set of other images can be reoriented along the 1st (reference) one.
+% They should be specified as "Other Images".
 % This is useful as the "unified segmentation" process is rather sensitive
 % to the starting orientation of the image.
 %
@@ -39,82 +39,44 @@ end
 if iscell(ref), ref = char(ref); end
 if iscell(other), other = char(other); end
 
-Vtempl = spm_vol(template);
 flags.regtype = 'mni';
 Nother = size(other,1);
-method = 1;
 
-switch method
-    case 1
-        % get image and smooth
-        ref = strtrim(ref);
-        spm_smooth(ref,'temp.nii',[8 8 8]);
-        vf = spm_vol('temp.nii');
-        % estimate reorientation
-        [M, scal] = spm_affreg(Vtempl,vf,flags);
-%         for citer = 2:8
-%             [M,scal] = spm_affreg(Vtempl,vf,flags,M,scal);
-%         end
+% get image and smooth (8mm for reference, no smoothing for template)
+ref = strtrim(ref);
+template = strtrim(template);
+Vref = spm_smoothto8bit(spm_vol(ref),8);
+Vtempl = spm_smoothto8bit(spm_vol(template),0);
 
-        M3 = M(1:3,1:3);
-        [u s v] = svd(M3);
-        M3 = u*v';
-        M(1:3,1:3) = M3;
-        % apply it on image to reorient
-        N = nifti(ref);
-        N.mat = M*N.mat;
-        create(N);
-        % apply it on other images
-        for cother = 1:Nother
-            fo = strtrim(other(cother,:));
-            if ~isempty(fo) && ~strcmp(ref,fo)
-                % allow case where :
-                % - noname was passed
-                % - name is same as the image used for the reorient
-                % => skip
-                N = nifti(fo);
-                N.mat = M*N.mat;
-                create(N);
-            end
-        end
-        % clean up
-        spm_get_space(deblank('temp.nii'),M*spm_get_space('temp.nii'));
-        %delete('temp.nii');
+% estimate reorientation
+[M, scal] = spm_affreg(Vtempl,Vref,flags); %#ok<*ASGLU>
+% extract rigid body part of the transform 
+[U,S,V] = svd(M(1:3,1:3));
+R = U*V'; % pure rotation without scaling
+T = R*(M(1:3,1:3)\M(1:3,4)); % = R(1:3,1:3)*M(1:3,1:3)^-1*M(1:3,4) translation corrected for scaling
+% replace in M
+M(1:3,1:3) = R; 
+M(1:3,4) = T;
+
+spm_progress_bar('Init',Nother+1,'Auto-Reorient to MNI space','volumes completed');
         
-    case 2
-        Niter = 8;
-        sep = 8./[1 2 4*ones(1,Niter)];
-        flags = struct('WG'      ,[]    ,...
-            'WF'      ,[]    ,...
-            'sep'     ,8     ,...
-            'regtype' ,'mni' ,...
-            'globnorm',0);
-        
-        VRef = spm_smoothto8bit(spm_vol(ref),8);
-        VTmp = spm_smoothto8bit(spm_vol(template),0);
-        VRef.pinfo(1:2,:) = VRef.pinfo(1:2,:)/spm_global(VRef);
-        VTmp.pinfo(1:2,:) = VTmp.pinfo(1:2,:)/spm_global(VTmp);
-        
-        [M,scal] = spm_affreg(VTmp,VRef,flags,eye(4));
-%         for citer = 2:Niter
-%             flags.sep = sep(citer);
-%             [M,scal] = spm_affreg(VTmp,VRef,flags,M,scal);
-%         end
-        [A,B,C] = svd(M(1:3,1:3)); R = A*C'; %#ok<ASGLU>
-        R(:,4) = R*(M(1:3,1:3)\M(1:3,4)); R(4,4) = 1;
-        
-        spm_progress_bar('Init',Nother,'Auto-Reorient to MNI space','volumes completed');
-        % apply reorientation to ref image
-        spm_get_space(deblank(ref),R*spm_get_space(deblank(ref)));
-        % apply reorientation to all the other images
-        for cother = 1:Nother
-            spm_get_space(deblank(other(cother,:)),...
-                R*spm_get_space(deblank(other(cother,:))));
-            spm_progress_bar('Set',cother);
-        end
-        spm_progress_bar('Clear');
-        
+% apply to reference image
+spm_get_space(ref,M*spm_get_space(ref));
+spm_progress_bar('Set',1);
+
+% apply to other images
+for cother = 1:Nother
+    fo = strtrim(other(cother,:));
+    if ~isempty(fo) && ~strcmp(ref,fo)
+        % allow case where :
+        % - noname was passed
+        % - name is same as the image used for the reorient
+        % => skip
+        spm_get_space(fo,M*spm_get_space(fo));
+    end
+    spm_progress_bar('Set',cother+1);
 end
+spm_progress_bar('Clear');        
 
 outfiles = cellstr(char(ref,other));
 

cbea7361961c03fa17058cc266c49cf5cb19ff27
ebalteau <e.balteau@ulg.ac.be>
2018-10-22 12:18:13 +0200

    Tracking Matlab version number (1)
    Matlab version number added in hmri_get_version, as well as
    last commit if available (optional, for developers).

--- hmri_get_version.m
+++ hmri_get_version.m
@@ -1,21 +1,23 @@
-function version = hmri_get_version
-% To retrieve the SHA1, author, date and message of the last commit of the
-% current branch of the repository and return the information as a string.
+function hmri_version = hmri_get_version
+% To retrieve the current hMRI-Toolbox version number from version.txt.
+% Additional information (SHA1, author, date and message of the last commit
+% of the current branch of the repository) can be retrieved using git from
+% Matlab (optional, see DEPENDENCIES below).
 % This script MUST be located in the root directory of the repository.
 % If the Toolbox has been copied whitout version tracking, the version can
 % only be retrieved if a version.txt file is already present in the root
-% directory of the Toolbox.
+% directory of the Toolbox. No additional commit number will be retrieved.
 %
 % DEPENDENCIES (NOT MANDATORY)
 % This script calls the git command using the MATLAB-git wrapper from
 % https://github.com/manur/MATLAB-git.git. The latter allows you to use
 % command line git instructions in Matlab (as long as Git is installed on
-% your computer!). Make sure that the git.m script is in the Matlab path to
-% execute this script. 
+% your computer! - https://git-scm.com/download). Make sure that the git.m
+% script is in the Matlab path to execute this script. 
 %
 % COMMAND LINE EQUIVALENT IN GIT BASH 
-% (to output the information into version.txt)
-% § git log -1 > version.txt
+% (to output the information into lastcommit.txt)
+% § git log -1 > lastcommit.txt
 %
 % ALTERNATIVELY, see version/hmri_get_version_readme.pdf for alternative
 % way to update the version.txt file using post-commit and post-merge
@@ -34,12 +36,14 @@ current_dir = pwd;
 cd(repos_dir);
 
 % initialise the output variable:
-version = [];
+hmri_version = [];
 
 try
     % execute the git command to output the current version into text file:
-    git log -1 > version.txt
+    git log -1 > lastcommit.txt
+    lastcommit = true;
 catch MExc %#ok<*NASGU>
+    lastcommit = false;
     % fprintf(1,['\nWARNING:\nEither or both MATLAB-git and git are not available on this machine.\n'...
     %    'The current version of the hMRI-Toolbox cannot be retrieved.\n'...
     %    'See ''help hmri_get_version'' for details about dependencies.\n'...     
@@ -49,27 +53,51 @@ end
 
 % version.txt file should be the following:
 version_fname = fullfile(repos_dir,'version.txt');
+% lastcommit.txt file should be the following:
+lastcommit_fname = fullfile(repos_dir,'lastcommit.txt');
 
 if ~exist(version_fname,'file')
     fprintf(1,'File %s does not exist.\nhMRI-Toolbox version unknown.\n\n', version_fname);
-    version = 'Unknown version';
+    hmri_version = 'Unknown hMRI version. File version.txt does not exist.';
 else
     % try to open the version.txt file:
     fid = fopen(version_fname,'r');
     if (fid~=-1)
+        % init version description
+        hmri_version = 'hMRI ';
         % read file content
         clin = fgets(fid);
         while (clin~=-1)
-            version = [version clin];
+            hmri_version = [hmri_version clin]; %#ok<AGROW>
             clin = fgets(fid);
         end
         fclose(fid);
         % fprintf(1,'Toolbox version:\n%s\n', version);
     else
         fprintf(1,'Cannot open file %s.\nhMRI-Toolbox version unknown.\n\n', version_fname);
-        version = 'Unknown version';
+        hmri_version = 'Unknown hMRI version. Cannot open file version.txt.';
     end
 end
+
+% add last commit if available 
+if lastcommit
+    % try to open the version.txt file:
+    fid = fopen(lastcommit_fname,'r');
+    if (fid~=-1)
+        % read file content
+        clin = fgets(fid);
+        while (clin~=-1)
+            hmri_version = [hmri_version clin]; %#ok<AGROW>
+            clin = fgets(fid);
+        end
+        fclose(fid);
+        % fprintf(1,'Toolbox version:\n%s\n', version);
+    end
+    delete(lastcommit_fname);
+end
+
+% add Matlab version for full version tracking
+hmri_version = sprintf('Matlab %s \n%s', version, hmri_version);
 
 % back to the current working directory
 cd(current_dir);

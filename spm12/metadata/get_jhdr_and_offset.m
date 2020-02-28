3700c549f9d72dba473873f973b632ca22c69813
Michael Krause <krause@mpib-berlin.mpg.de>
2020-02-28 21:24:31 +0100

    Fix wrong calculation of extended header size
    Simulate a write with fprintf('%s') to get actual byte size of the json
    string representation instead of just using length().
    
    Fixes #8

--- spm12/metadata/get_jhdr_and_offset.m
+++ spm12/metadata/get_jhdr_and_offset.m
@@ -1,9 +1,10 @@
-function [jhdr, offset] = get_jhdr_and_offset(hdr)
+function [jhdr, jhdr_size, offset] = get_jhdr_and_offset(hdr)
 %==========================================================================
 % FORMAT
-% [jhdr, offset] = get_jhdr_and_offset(hdr)
+% [jhdr, jhdr_size, offset] = get_jhdr_and_offset(hdr)
 % hdr       a Matlab structure
 % jhdr      the JSONified Matlab structure
+% jhdr_size size of the JSON header encoding in bytes
 % offset    the offset at which image data start in the nifti file
 %==========================================================================
 % Written by
@@ -13,9 +14,14 @@ function [jhdr, offset] = get_jhdr_and_offset(hdr)
 % JSONify the Matlab structure
 jhdr = spm_jsonwrite(hdr,struct('indent','\t'));
 
+% simulate header write, fprintf() returns actual bytes written. length()
+% doesn't work here because characters might be larger than one byte (UTF-8)
+fid = fopen(tempname, 'w');
+jhdr_size = fprintf(fid, '%s', jhdr);
+fclose(fid);
 % the length of the extension includes two 32-bit numbers = 8 bytes (the
 % size of the extension itself and the ID of the extension):
-ext_hdr_size = length(jhdr)+8;
+ext_hdr_size = jhdr_size+8;
 
 % the offset must be >352+ext_hdr_size AND a multiple of 16
 offset = ceil((352+ext_hdr_size)/16)*16;

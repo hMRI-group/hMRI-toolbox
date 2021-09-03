66635d899d597b1d92bc954e03080b5064dd253c
Luke Edwards <ledwards@cbs.mpg.de>
2021-09-03 14:16:05 +0200

    Improving function documentation and adding some imput checking.

--- hmri_calc_R1.m
+++ hmri_calc_R1.m
@@ -1,10 +1,14 @@
 % Calculate R1 map from PDw and T1w data.
 % 
-% Inputs of form:
-%   {PDw,T1w}.data (array)
-%   {PDw,T1w}.fa   (rad)
-%   {PDw,T1w}.TR   (s or ms)
-%   {PDw,T1w}.B1   (actual fa / nominal fa)
+% Input:
+%   PDw and T1w are structs with the fields:
+%     {PDw,T1w}.data (array of signals)
+%     {PDw,T1w}.fa   (nominal flip angle, rad)
+%     {PDw,T1w}.TR   (repetition time, s or ms)
+%     {PDw,T1w}.B1   (the ratio: actual fa / nominal fa)
+%
+%   small_angle_approx (bool which determines whether to use the small
+%     angle approximation [true] or to use a more exact formula [false])
 %
 % Output:
 %   R1 (in reciprocal of TR units)
@@ -21,6 +25,10 @@ else
     PDw.t=PDw.B1.*PDw.fa;
     T1w.t=T1w.B1.*T1w.fa;
 end
+
+assert(all(size(PDw.data)==size(T1w.data)),'hmri:inputArraySize','PDw.data and T1w.data must be the same size!')
+assert(PDw.TR>0,'hmri:TR','PDw.TR must be positive!')
+assert(T1w.TR>0,'hmri:TR','T1w.TR must be positive!')
 
 R1=0.5*( PDw.data.*PDw.t/PDw.TR - T1w.data.*T1w.t/T1w.TR )...
     ./( T1w.data./T1w.t - PDw.data./PDw.t );

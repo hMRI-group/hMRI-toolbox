ae39b662e2b5119424683bc7317a85086ce994d4
Christophe Phillips <c.phillips@uliege.be>
2022-03-10 18:02:21 +0100

    Just typos in help parts

--- tbx_scfg_hmri_QUIQI_check.m
+++ tbx_scfg_hmri_QUIQI_check.m
@@ -8,9 +8,9 @@ function quiqi_check=tbx_scfg_hmri_QUIQI_check
 % with respect to their corresponfing MDI value
 %
 %_______________________________________________________________________
-% Nad?ge Corbin
+% Nadege Corbin
 % 2021.03.30
-% Centre de R?sonance Magn?tique des Syst?mes Biologiques, Bordeaux, France
+% Centre de Resonance Magnetique des Systemes Biologiques, Bordeaux, France
 % ======================================================================
 
 % ---------------------------------------------------------------------
@@ -19,7 +19,7 @@ function quiqi_check=tbx_scfg_hmri_QUIQI_check
 spm_mat_file         = cfg_files;
 spm_mat_file.tag     = 'spm_mat_file';
 spm_mat_file.name    = 'SPM.mat file';
-spm_mat_file.help    = {'Select the SPM.mat file containing the design of the model '};
+spm_mat_file.help    = {'Select the SPM.mat file containing the design of the model.'};
 spm_mat_file.ufilter = '^SPM.mat$';
 spm_mat_file.num     = [1 1];
 
@@ -29,13 +29,13 @@ pow.name    = 'Fit power ';
 pow.val     = {[4]};
 pow.strtype = 'e';
 pow.num     = [1 1];
-pow.help    = {['Specify the power of  the polynomial fit ']};
+pow.help    = {'Specify the power of  the polynomial fit.'};
 
 
 quiqi_check        = cfg_exbranch;
 quiqi_check.tag     = 'quiqi_check';
 quiqi_check.name    = 'QUIQI CHECK';
 quiqi_check.val     = { spm_mat_file pow};
-quiqi_check.help    = {'Spatial variance of the residuals of each subject is plotted',...
+quiqi_check.help    = {'Spatial variance of the residuals of each subject is plotted ',...
  'with respect to their corresponfing MDI value'};
 quiqi_check.prog    = @hmri_quiqi_check;

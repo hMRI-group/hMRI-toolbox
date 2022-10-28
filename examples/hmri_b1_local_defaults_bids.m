d103d3db7a05acf75f3c069163f25b6b689292d8
Luke Edwards <ledwards@cbs.mpg.de>
2022-10-28 22:08:46 +0200

    Document 3D-EPI SE/STE B1 input ordering

--- examples/hmri_b1_local_defaults_bids.m
+++ examples/hmri_b1_local_defaults_bids.m
@@ -36,5 +36,6 @@ global hmri_def
 % 'i3D_EPI'
 % b1-validation
 hmri_def.b1map.i3D_EPI.b1validation.checkTEs = true; % input validation using image TEs. Assumes SE has shorter TE than STE in metadata (qMRI-BIDS assumption). Disabled by default as this assumption is not valid for the metadata in the DICOMs from some sequences.
+hmri_def.b1map.i3D_EPI.b1validation.useBidsFlipAngleField = true; % qMRI-BIDS support: read flip angles from BIDS metadata. Requires this metadata to be set appropriately!
 
 end

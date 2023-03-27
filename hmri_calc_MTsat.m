dbf9264e5275a5dd620f25a23c189a87112affce
Luke Edwards <ledwards@cbs.mpg.de>
2023-03-27 21:03:53 +0200

    Improve documentation

--- hmri_calc_MTsat.m
+++ hmri_calc_MTsat.m
@@ -2,18 +2,29 @@ function MTsat=hmri_calc_MTsat(MTw,A,R1)
 %hmri_calc_MTsat Calculate MTsat from MTw data.
 % 
 % Input:
-%     MTw.data (array of MT-weighted signals)
-%     MTw.fa   (nominal flip angle, rad)
-%     MTw.TR   (repetition time, s or ms)
-%     MTw.B1   (array of B1 ratios: either 1 [classic MTsat] or actual fa / nominal fa [calibrated MTsat for high FA])
-%     A        (unnormalised PD map, must not have been scaled relative to MTw.data)
-%     R1       (R1 estimates, in reciprocal units of MTw.TR)
+%   MTw.data (array of MT-weighted signals)
+%   MTw.fa   (nominal excitation flip angle, rad)
+%   MTw.TR   (repetition time, s or ms)
+%   MTw.B1   (array of B1 ratios: either 1 [classic MTsat] or actual fa / nominal fa [calibrated MTsat for high FA])
+%   A        (unnormalised PD map, must not have been scaled relative to MTw.data)
+%   R1       (R1 estimates, in reciprocal units of MTw.TR)
 %
 % Output:
 %   MTsat (in percent units [p.u.])
 %
 % Examples:
+%   % Procedure from Helms (2008) correcting for B1 to second order within the small flip angle regime by setting B1 = 1
+%   A_for_MTsat = hmri_calc_A(struct('data',data_pdw,'fa',fa_pdw,'TR',tr_pdw,'B1',1),...
+%                             struct('data',data_t1w,'fa',fa_t1w,'TR',tr_t1w,'B1',1), true);
+%   R1_for_MTsat = hmri_calc_R1(struct('data',data_pdw,'fa',fa_pdw,'TR',tr_pdw,'B1',1),...
+%                               struct('data',data_t1w,'fa',fa_t1w,'TR',tr_t1w,'B1',1), true);
+%   MTsat = hmri_calc_MTsat(struct('data',data_mtw,'fa',fa_mtw,'TR',tr_mtw,'B1',1), A_for_MTsat, R1_for_MTsat);
+%
 % References:
+%   Helms, et al. (2008), "High-resolution maps of magnetization transfer with inherent correction for RF inhomogeneity 
+%       and T1 relaxation obtained from 3D FLASH MRI". Magn. Reson. Med. https://doi.org/10.1002/mrm.21732
+%   Tabelow, et al. (2019), "hMRI – A toolbox for quantitative MRI in neuroscience and clinical research". Neuroimage 
+%       https://doi.org/10.1016/j.neuroimage.2019.01.029
 
 if isempty(MTw.B1), MTw.B1=1; end
 
--- hmri_correct_MTsat.m
+++ hmri_correct_MTsat.m
@@ -2,9 +2,8 @@ function MTsat=hmri_correct_MTsat(MTsat,B1)
 %hmri_correct_MTsat Correct MTsat using B1 and a heuristic model.
 % 
 % Input:
-%   PDw and T1w are structs with the fields:
-%     MTsat (array of MTsat estimates)
-%     B1    (array of B1 ratios: actual fa / nominal fa)
+%   MTsat (array of MTsat estimates)
+%   B1    (array of B1 ratios: actual fa / nominal fa)
 %
 % Output:
 %   MTsat (in percent units [p.u.])

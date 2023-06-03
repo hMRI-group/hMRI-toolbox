33bc033db327fd1b1d0061c644cf6b43cebbf300
ledwards <ledwards@cbs.mpg.de>
2023-06-03 21:50:47 +0200

    Add two more tests, fix revealed bug in AFI logging

--- hmri_code_tests/hmri_create_b1map_test.m
+++ hmri_code_tests/hmri_create_b1map_test.m
@@ -15,10 +15,10 @@ classdef hmri_create_b1map_test < matlab.unittest.TestCase
     methods (Test)
         
         %% Test Functions
-        function test_afi_hmri(testCase)
+        function test_afi(testCase)
             
             % Test that AFI B1 mapping works with synthetic data like from 
-            % hMRI toolbox DICOM conversion
+            % BIDS and hMRI toolbox DICOM conversion
 
             V=testCase.phantomStruct;
 
@@ -36,29 +36,20 @@ classdef hmri_create_b1map_test < matlab.unittest.TestCase
             A=1e3;
             R1=1/1e3; % 1/ms
 
-            bids.FlipAngle=alphanom;
             
             % TR1
-            S1=abs(hmri_test_utils.dualTRernstd(B1*alphanom,TR1,TR2,R1)); 
+            S1=abs(A*hmri_test_utils.dualTRernstd(B1*alphanom,TR1,TR2,R1)); 
             fname1=spm_file(testCase.phantomStruct.fname,'suffix','_AFI_TR1'); 
             V.fname=fname1;
             V.descrip=sprintf('Synthetic AFI data for hMRI toolbox testing TR=%dms/TE=%dms/FA=%ddeg',TR1,0,alphanom);
             spm_write_vol(V,S1);
-            bids.RepetitionTimeExcitation=TR1;
-            f=fopen(spm_file(fname1,'ext','.json'),'w');
-            fprintf(f,jsonencode(bids));
-            fclose(f);
 
             % TR2
-            S2=abs(hmri_test_utils.dualTRernstd(B1*alphanom,TR2,TR1,R1));
+            S2=abs(A*hmri_test_utils.dualTRernstd(B1*alphanom,TR2,TR1,R1));
             fname2=spm_file(testCase.phantomStruct.fname,'suffix','_AFI_TR2'); 
             V.fname=fname2;
             V.descrip=sprintf('Synthetic AFI data for hMRI toolbox testing TR=%dms/TE=%dms/FA=%ddeg',TR2,0,alphanom);
             spm_write_vol(V,S2);
-            bids.RepetitionTimeExcitation=TR2;
-            f=fopen(spm_file(fname2,'ext','.json'),'w');
-            fprintf(f,jsonencode(bids));
-            fclose(f);
 
             %% Create SPM batch job to run
             config=fullfile(fileparts(mfilename('fullpath')),'testconfig','hmri_b1_local_defaults_test_afi_bids.m');
@@ -71,13 +62,49 @@ classdef hmri_create_b1map_test < matlab.unittest.TestCase
             matlabbatch{1}.spm.tools.hmri.create_B1.subj.b1_type.i3D_AFI.b1parameters.b1defaults = '<UNDEFINED>';
             matlabbatch{1}.spm.tools.hmri.create_B1.subj.popup = false;
             
-            %% Run SPM batch job
+            %% Run SPM batch job and evaluate result with bids sidecar file
+            bids.FlipAngle=alphanom;
+
+            bids.RepetitionTimeExcitation=TR1;
+            f=fopen(spm_file(fname1,'ext','.json'),'w');
+            fprintf(f,jsonencode(bids));
+            fclose(f);
+
+            bids.RepetitionTimeExcitation=TR2;
+            f=fopen(spm_file(fname2,'ext','.json'),'w');
+            fprintf(f,jsonencode(bids));
+            fclose(f);
+
             spm('defaults', 'FMRI');
             out = spm_jobman('run', matlabbatch, inputs{:});
             
-            %% Evaluate result
             B1est = spm_read_vols(spm_vol(out{1}.subj.B1map{1}))/100;
             assertEqual(testCase, B1est, B1, 'RelTol', 1e-2)
+
+            %% Run SPM batch job and evaluate result with hmri-like sidecar file
+            hmri.acqpar.FlipAngle=alphanom;
+            hmri.acqpar.alTR=[TR1,TR2];
+
+            f=fopen(spm_file(fname1,'ext','.json'),'w');
+            fprintf(f,jsonencode(hmri));
+            fclose(f);
+
+            f=fopen(spm_file(fname2,'ext','.json'),'w');
+            fprintf(f,jsonencode(hmri));
+            fclose(f);
+
+            spm('defaults', 'FMRI');
+            out = spm_jobman('run', matlabbatch, inputs{:});
+            
+            B1est = spm_read_vols(spm_vol(out{1}.subj.B1map{1}))/100;
+            assertEqual(testCase, B1est, B1, 'RelTol', 1e-2)
+
+            %% Test that appropriate warning is given if images given in wrong order
+            inputs_wrong = inputs;
+            inputs_wrong{1} = {fname2; fname1}; % Create B1 map: B1 input - cfg_files
+
+            spm('defaults', 'FMRI');
+            verifyWarning(testCase,@() spm_jobman('run', matlabbatch, inputs_wrong{:}),'hmri:afiTooManyImag')
         end
         
         

888ee5fe8c2f9c42da18ebe129d39e953d50674d
Luke Edwards <ledwards@cbs.mpg.de>
2023-12-17 12:39:56 +0100

    Print orientation check output to console when there are no output arguments

--- hmri_check_orientations.m
+++ hmri_check_orientations.m
@@ -8,6 +8,8 @@ function [sts, str] = hmri_check_orientations(V, verbose)
 % str     - string describing status, empty if OK
 %
 % When used without LHS, this function throws an error accordingly.
+% If zero or one output arguments are requested, then str is output
+% to the console.
 %__________________________________________________________________________
 % Adapted from John Ashburner's spm_check_orientations.m
 % to save all console output in str
@@ -21,45 +23,45 @@ if nargin < 2, verbose = true; end
 dims = cat(1,V.dim);
 if any(any(diff(dims,1,1),1))
     sts = false;
-    str = sprintf('%s\n\t** The images do not all have the same dimensions **\n',str);
+    strdim = sprintf('\n\t** The images do not all have the same dimensions **\n');
     if verbose
-        str = sprintf('%sThe function assumes that a voxel in one image corresponds with\n',str);
-        str = sprintf('%sthe same  voxel in another.   This  is not a safe assumption if\n',str);
-        str = sprintf('%sthe  image dimensions differ.   Please  ensure  that  you  have\n',str);
-        str = sprintf('%sprocessed all the image data in the same way (eg. check spatial\n',str);
-        str = sprintf('%snormalisation bounding-boxes, voxel-sizes etc).\n',str);
-        str = sprintf('%sHere are the dimensions of the image volumes.  This list can be\n',str);
-        str = sprintf('%sused to determine which file(s) are causing the problem.\n\n',str);
+        strdim = sprintf('%sThe function assumes that a voxel in one image corresponds with\n',strdim);
+        strdim = sprintf('%sthe same  voxel in another.   This  is not a safe assumption if\n',strdim);
+        strdim = sprintf('%sthe  image dimensions differ.   Please  ensure  that  you  have\n',strdim);
+        strdim = sprintf('%sprocessed all the image data in the same way (eg. check spatial\n',strdim);
+        strdim = sprintf('%snormalisation bounding-boxes, voxel-sizes etc).\n',strdim);
+        strdim = sprintf('%sHere are the dimensions of the image volumes.  This list can be\n',strdim);
+        strdim = sprintf('%sused to determine which file(s) are causing the problem.\n\n',strdim);
         for i=1:numel(V)
-            str = sprintf('%s[%d %d %d]  %s\n', str, V(i).dim, V(i).fname);
+            strdim = sprintf('%s[%d %d %d]  %s\n', strdim, V(i).dim, V(i).fname);
         end
-        str = sprintf('%s\n',str);
+        strdim = sprintf('%s\n',strdim);
     end
+    str = sprintf('%s%s',str,strdim);
+    if nargout<2, disp(strdim); end
     if ~nargout, error('The dimensions must be identical for this procedure.'); end
 end
 
 matx = reshape(cat(3,V.mat),[16,numel(V)]);
 if any(any(abs(diff(matx,1,2))>1e-4))
     sts = false;
-    str = sprintf('%s\n\t** The images do not all have same orientation and/or voxel sizes **',str);
+    strori = sprintf('\n\t** The images do not all have same orientation and/or voxel sizes **');
     if verbose
-        str = sprintf('%sThe function assumes that a voxel in one image  corresponds exactly\n',str);
-        str = sprintf('%swith  the same voxel in another.   This is not a safe assumption if\n',str);
-        str = sprintf('%sthe orientation information  in the headers or .mat files says that\n',str);
-        str = sprintf('%sthe images are oriented differently. Please ensure that you process\n',str);
-        str = sprintf('%sall data correctly. For example, you may have realigned the images,\n',str);
-        str = sprintf('%sbut not actually resliced them to be in voxel-wise alignment.\n',str);
-        str = sprintf('%sHere are the orientation matrices of the image volumes.   This list\n',str);
-        str = sprintf('%scan be used to determine which file(s) are causing the problem.\n\n',str);
+        strori = sprintf('%sThe function assumes that a voxel in one image  corresponds exactly\n',strori);
+        strori = sprintf('%swith  the same voxel in another.   This is not a safe assumption if\n',strori);
+        strori = sprintf('%sthe orientation information  in the headers or .mat files says that\n',strori);
+        strori = sprintf('%sthe images are oriented differently. Please ensure that you process\n',strori);
+        strori = sprintf('%sall data correctly. For example, you may have realigned the images,\n',strori);
+        strori = sprintf('%sbut not actually resliced them to be in voxel-wise alignment.\n',strori);
+        strori = sprintf('%sHere are the orientation matrices of the image volumes.   This list\n',strori);
+        strori = sprintf('%scan be used to determine which file(s) are causing the problem.\n\n',strori);
         for i=1:numel(V)
-            str = sprintf('%s[%g %g %g %g; %g %g %g %g; %g %g %g %g]  %s\n',...
-                str, V(i).mat(1:3,:)', V(i).fname);
+            strori = sprintf('%s[%g %g %g %g; %g %g %g %g; %g %g %g %g]  %s\n',...
+                strori, V(i).mat(1:3,:)', V(i).fname);
         end
-        str = sprintf('%s\n',str);
+        strori = sprintf('%s\n',strori);
     end
+    str = sprintf('%s%s',str,strori);
+    if nargout<2, disp(strori); end
     if ~nargout, error('The orientations etc must be identical for this procedure.'); end
-end
-
-if nargout<2
-    disp(str);
 end

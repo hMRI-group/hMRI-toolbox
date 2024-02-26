f11ad6f2c4d6650d7262bc9b320f4796be410297
BarisUgurcan <beu4@cornell.edu>
2024-02-26 12:58:06 +0100

    error if toolbox required by optional argument

--- hmri_calc_R2s.m
+++ hmri_calc_R2s.m
@@ -156,6 +156,12 @@ switch lower(method)
         [R2s0,A0]=hmri_calc_R2s(weighted_data,initmethod);
         
         if ~exist('opt','var')
+            %lsqcurvefit uses toolbox check for it:
+            [status,~] = license('checkout', 'Optimization_toolbox');
+            if status==0
+                error("The methods: 'nlls_ols','nlls_wls1','nlls_wls2','nlls_wls3' require Optimization Toolbox, " + ...
+                    "try another method (e.g. OLS) which does not use Optimization Toolbox.")
+            end
             opt=optimset('lsqcurvefit');
         end
         opt=optimset(opt,'Display','off');
@@ -167,7 +173,13 @@ switch lower(method)
         end
         
         expDecay=@(x,D) (D(:,2:end)*x(2:end)).*exp(x(1)*D(:,1));
-        
+      
+        %lsqcurvefit uses toolbox check for it, before the loop:
+        [status,~] = license('checkout', 'Optimization_toolbox');
+            if status==0
+                error("The methods: 'nlls_ols','nlls_wls1','nlls_wls2','nlls_wls3' require Optimization Toolbox, " + ...
+                    "try another method (e.g. OLS) which does not use Optimization Toolbox.")
+            end
         % Loop over voxels
         parfor n=1:size(y,2)
             beta(:,n)=lsqcurvefit(@(x,D)expDecay(x,D),beta0(:,n),D,y(:,n),[],[],opt);

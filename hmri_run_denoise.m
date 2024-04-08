988daedb31c4d890b44a3b4aef2298f35959031b
BarisUgurcan <beu4@cornell.edu>
2024-04-08 14:22:59 +0200

    add folder creation and log file display warnings in case of repeat processing

--- hmri_run_denoise.m
+++ hmri_run_denoise.m
@@ -13,4 +13,55 @@ catch
 end
 
 
+% save outpath as default for this job
+hmri_get_defaults('outdir',outpath);
+
+% Directory structure for results:
+% <output directory>/Results
+% <output directory>/Results/Supplementary
+% If repeated runs, <output directory> is replaced by <output
+% directory>/Run_xx to avoid overwriting previous outputs.
+ 
+% define a directory for final results
+% RESULTS contains the final denoised maps
+% Supplementary containes additonal info and log
+respath = fullfile(outpath, 'Results');
+newrespath = false;
+if exist(respath,'dir')
+    index = 1;
+    tmpoutpath = outpath;
+    while exist(tmpoutpath,'dir')
+        index = index + 1;
+        tmpoutpath = fullfile(outpath,sprintf('Run_%0.2d',index));
+    end
+    outpath = tmpoutpath;
+    mkdir(outpath);
+    respath = fullfile(outpath, 'Results');
+    newrespath = true;
+end
+if ~exist(respath,'dir'); mkdir(respath); end
+supplpath = fullfile(outpath, 'Results', 'Supplementary');
+if ~exist(supplpath,'dir'); mkdir(supplpath); end
+
+% save all these paths in the job.subj structure
+job.subj.path.dnrespath = respath; 
+job.subj.path.respath = respath;
+job.subj.path.supplpath = supplpath;
+
+% save log file location
+job.subj.log.logfile = fullfile(supplpath, 'hMRI_denoising_logfile.log');
+job.subj.log.flags = struct('LogFile',struct('Enabled',true,'FileName','hMRI_denoising_logfile.log','LogDir',supplpath), ...
+    'PopUp',job.subj.popup,'ComWin',true);
+flags = job.subj.log.flags;
+flags.PopUp = false;
+hmri_log(sprintf('\t============ DENOISING MODULE - %s.m (%s) ============', mfilename, datestr(now)),flags);
+
+if newrespath
+    hmri_log(sprintf(['WARNING: existing results from previous run(s) were found, \n' ...
+        'the output directory has been modified. It is now:\n%s\n'],outpath),job.subj.log.flags);
+else
+    hmri_log(sprintf('INFO: the output directory is:\n%s\n',outpath),flags);
+end
+
+
 end

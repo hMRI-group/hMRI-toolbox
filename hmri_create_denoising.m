d7aacd5b65991d90204a724489203ea0341b89fb
BarisUgurcan <beu4@cornell.edu>
2024-04-08 16:01:53 +0200

    exec chosen denoise protocol and get output

--- hmri_create_denoising.m
+++ hmri_create_denoising.m
@@ -5,4 +5,12 @@ jobsubj = job.subj;
 denoisedout = get_denoising_params(jobsubj);
 protocolfield = fieldnames(jobsubj.denoisingtype);
 denoising_protocol = protocolfield{1};
+
+%execute the chosen denoising method and define output
+switch denoising_protocol
+    case 'lcpca_denoise'
+[output_mag, output_phase] = hmri_calc_lcpcadenoise(denoisedout);
+varargout{1} = output_mag;
+varargout{2} = output_phase;
+end
 end

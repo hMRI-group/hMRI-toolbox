4468b7a49721af37be35e2bbcf2cf035f51ec441
ledwards <ledwards@cbs.mpg.de>
2024-06-14 15:08:28 +0200

    Remove pre-initialisation of denoising input data to avoid confusion

--- config/local/hmri_local_denoising_defaults.m
+++ config/local/hmri_local_denoising_defaults.m
@@ -9,13 +9,10 @@
 % init the global variable which carries the params
 global hmri_def;
 
-% Enter denoising default values as: hmri_def.denoising.(denoising-protocol).(default-value)
+% Enter denoising default values as: hmri_def.denoising.(denoising-protocol).(denoising-parameter)
 
 % The default values for lcpca denoising protocol
 % all optional parameters turned off
-hmri_def.denoising.lcpca_denoise.mag_input   = {}; % required-null initialize here input with GUI
-hmri_def.denoising.lcpca_denoise.phase_input = {}; % optional-null initialize here input with GUI
-hmri_def.denoising.lcpca_denoise.output_path = ''; % required-null initialize here input with GUI
 hmri_def.denoising.lcpca_denoise.min_dimension =  0; % required-initialize here
 hmri_def.denoising.lcpca_denoise.max_dimension = -1; % required-initialize here
 hmri_def.denoising.lcpca_denoise.unwrap = false; % optional

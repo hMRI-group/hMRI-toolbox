f4df6b52a29664810c7026094b5534d44b1cc93d
Luke Edwards <ledwards@cbs.mpg.de>
2024-07-03 16:25:21 +0200

    Add documentation of denoising config parameters from Nighres documentation

--- config/hmri_denoising_defaults.m
+++ config/hmri_denoising_defaults.m
@@ -36,12 +36,11 @@ global hmri_def;
 % Enter denoising default values as: hmri_def.denoising.(denoising-protocol).(denoising-parameter)
 
 % The default values for lcpca denoising protocol
-% all optional parameters turned off
-hmri_def.denoising.lcpca_denoise.min_dimension =  0; % required-initialize here
-hmri_def.denoising.lcpca_denoise.max_dimension = -1; % required-initialize here
-hmri_def.denoising.lcpca_denoise.unwrap = false; % optional
-hmri_def.denoising.lcpca_denoise.rescale_phs = false; % optional
-hmri_def.denoising.lcpca_denoise.process_2d = false; % optional
-hmri_def.denoising.lcpca_denoise.use_rmt = false; % optional
+hmri_def.denoising.lcpca_denoise.min_dimension =  0; % minimum number of kept PCA components
+hmri_def.denoising.lcpca_denoise.max_dimension = -1; % maximum number of kept PCA components (-1 for all components)
+hmri_def.denoising.lcpca_denoise.unwrap = true; % whether to unwrap the phase data of keep it as is
+hmri_def.denoising.lcpca_denoise.rescale_phs = true; % whether to rescale the phase data or keep it as is, assuming radians
+hmri_def.denoising.lcpca_denoise.process_2d = false; % whether to denoise in 2D, for instance when acquiring a thin slab of data
+hmri_def.denoising.lcpca_denoise.use_rmt = false; % whether to use random matrix theory rather than noise fitting to estimate the noise threshold
 
 end

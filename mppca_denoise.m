f697cc05bc3d3045e5fced9fb71f0d930a608015
BarisUgurcan <beu4@cornell.edu>
2024-10-01 14:03:53 +0200

    reorganize mppca denoising files into single function

--- mppca_denoise.m
+++ mppca_denoise.m
@@ -1,10 +1,10 @@
-function [denoised,S2,P] = mppca_denoise(image,window,mask)
 %{
 REFERENCES:
 --Veraart et al., NeuroImage (2016) 142, p 394-406 (https://doi.org/10.1016/j.neuroimage.2016.08.016)  
 --Does, MD al. Evaluation of principal component analysis image denoising on multi‐exponential MRI relaxometry. Magn Reson Med. 2019; 81: 3503– 3514. https://doi.org/10.1002/mrm.27658
 --https://github.com/Neurophysics-CFIN/MP-PCA-Denoising/tree/master
 %}
+function [denoised,S2,P] = mppca_denoise(image,window,mask)
 % MP-PCA denoising
 %
 % input:
@@ -87,5 +87,75 @@ P = reshape(P,dimsOld(1:end-1));
 S2 = reshape(S2,dimsOld(1:end-1));
 
 
+end
+
+
+% Want first image indices to discriminate between pixels and last
+% dimension to hold data for each pixel. This function puts the image data
+% on the form: row x col x slice x rest.
+function [image,mask] = mppca_imageAssert(image,mask)
+
+dims = size(image);
+assert(length(dims)<=4,'image data array must not have more than 4 dimensions')
+
+% construct mask if not given
+if numel(mask)==0
+    if sum(dims~=1)==1
+        mask = true;
+    else
+        mask = true([dims(1:end-1) 1]);
+    end
+end
+maskDims = size(mask);
+
+% voxel count must match
+assert(numel(mask)==prod(dims(1:end-1)),'mask dimensions does not match image dimensions')
+
+% if image only contains data from single pixel
+if (all(dims(1:end-1)==1) && length(dims)<4) || (dims(1)>1 && dims(2)==1 && length(dims)<3) % (1xn || 1x1xn) || (nx1)
+    assert(numel(mask)==1,'mask dimensions does not match image dimensions')
+    dummy(1,1,1,:) = image;
+    image = dummy;
+end
+
+% if image is on form rxcxn
+if length(dims)==3
+    assert(all(maskDims==dims(1:end-1)),'mask dimensions does not match image dimensions')
+    dummy(:,:,1,:) = image;
+    image = dummy;
+end
+
+% if image is on form rxn
+if length(dims)==2 && dims(2)~=1
+    assert(all(maskDims(1)==dims(1)),'mask dimensions does not match image dimensions')
+    dummy(:,1,1,:) = image;
+    image = dummy;
+end
+end
+
+
+% X: denoised matrix, s2: original noise variance, p: number of signal components, s2_after: noise variance after denoising
+function [X,s2,p,s2_after] = mppca_denoiseMatrix(X) 
+M = size(X,1);
+N = size(X,2);
+if M<N
+    [U,lambda] = eig(X*X','vector');
+else
+    [U,lambda] = eig(X'*X,'vector');
+end
+[lambda,order] = sort(lambda,'descend');
+U = U(:,order);
+csum = cumsum(lambda,'reverse');
+p = (0:length(lambda)-1)';
+p = -1 + find((lambda-lambda(end)).*(M-p).*(N-p) < 4*csum*sqrt(M*N),1);
+if p==0
+    X = zeros(size(X));
+elseif M<N
+    X = U(:,1:p)*U(:,1:p)'*X;
+else
+    X = X*U(:,1:p)*U(:,1:p)';
+end
+s2 = csum(p+1)/((M-p)*(N-p));
+s2_after = s2 - csum(p+1)/(M*N);
 end
 

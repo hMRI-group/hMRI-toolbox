b9a95e789ea1a38e4f38ed7b2cba304219baf72b
ledwards <ledwards@cbs.mpg.de>
2026-08-05 15:43:52 +0200

    Fix some documentation and formatting in error map code

--- hmri_calc_dMT.m
+++ hmri_calc_dMT.m
@@ -1,8 +1,6 @@
 function dMT = hmri_calc_dMT(SPD,ST1,SMT,dSPD,dST1,dSMT,alpha_PD,alpha_T1,alpha_MT,TRPD,TRT1,TRMT,small_angle_approximation)
 % Calculate propagation of uncertainty for MT map
 % (https://en.wikipedia.org/wiki/Propagation_of_uncertainty).
-% Taking the total differential from Eq. A9 in Tabelow et al., NI, 2019
-% gives:
 % S.Mohammadi 06.09.2019
 %
 % In:
@@ -12,20 +10,21 @@ function dMT = hmri_calc_dMT(SPD,ST1,SMT,dSPD,dST1,dSMT,alpha_PD,alpha_T1,alpha_
 % dSPD          - residual of mono-exponential fit of PDw signal
 % dST1          - residual of mono-exponential fit of T1w signal
 % dSMT          - residual of mono-exponential fit of MTw signal
-% alpha_PD      - flip angle of T1w signal
+% alpha_PD      - flip angle of PDw signal
+% alpha_T1      - flip angle of T1w signal
 % alpha_MT      - flip angle of MTw signal
-% TRPD          - repitition time of PDw signal
-% TRT1          - repitition time of T1w signal
-% TRMT          - repitition time of MTw signal
-% small_angle_approximation - Switch to turn off small-angle approximation
-% 
+% TRPD          - repetition time of PDw signal
+% TRT1          - repetition time of T1w signal
+% TRMT          - repetition time of MTw signal
+% small_angle_approximation - switch to turn off small-angle approximation
+%
 % Out:
 % dMT           - error for MT in [a.u.]
 %
 % References:
-%   Mohammadi et al. NeuroImage (2022), "Error quantification in 
-%     multi-parameter mapping facilitates robust estimation and enhanced 
-%     group level sensitivity." 
+%   Mohammadi et al. NeuroImage (2022), "Error quantification in
+%     multi-parameter mapping facilitates robust estimation and enhanced
+%     group level sensitivity."
 %     https://doi.org/10.1016/j.neuroimage.2022.119529
 
 % We do not scale the flip angles by fT, because that would be inconsistent
@@ -49,12 +48,12 @@ dMT = sqrt( dMTdSPD.^2 .* dSPD.^2 + dMTdST1.^2 .* dST1.^2 + dMTdSMT.^2 .* dSMT.^
 end
 
 function d = dMT_by_dS1(S1,S2,SMT,alpha1,alpha2,alphaMT,TR1,TR2,TRMT)
-% Derivative of dual flip-angle A (PD) estimate with respect to first 
-% weighted signal (S1). Because of symmetry in the R1 and A calculations, 
-% the derivative with respect to the second weighted signal can be computed 
+% Derivative of MTsat estimate with respect to first weighted signal (S1,
+% i.e. SPD). Because of symmetry in the R1 and A calculations, the
+% derivative with respect to the second weighted signal can be computed
 % by permuting labels.
 %
-% Can be derived using: 
+% Can be derived using:
 %   syms S1 alpha1 S2 alpha2 SMT alphaMT
 %   syms TR1 TR2 TRMT positive
 %   A = hmri_calc_A(struct('data',S1,'fa',alpha1,'TR',TR1,'B1',1),struct('data',S2,'fa',alpha2,'TR',TR2,'B1',1),true);
@@ -67,10 +66,9 @@ d = S2.*TRMT.*alpha1.*(TR2.*alpha1.^2 - TR1.*alpha2.^2).*(S2.*alphaMT - SMT.*alp
 end
 
 function d = dMT_by_dSMT(S1,S2,SMT,alpha1,alpha2,alphaMT,TR1,TR2,TRMT)
-% Derivative of dual flip-angle A (PD) estimate with respect to MT 
-% weighted signal (SMT).
+% Derivative of MTsat estimate with respect to MT weighted signal (SMT).
 %
-% Can be derived using: 
+% Can be derived using:
 %   syms S1 alpha1 S2 alpha2 SMT alphaMT
 %   syms TR1 TR2 TRMT positive
 %   A = hmri_calc_A(struct('data',S1,'fa',alpha1,'TR',TR1,'B1',1),struct('data',S2,'fa',alpha2,'TR',TR2,'B1',1),true);
--- hmri_calc_dPD.m
+++ hmri_calc_dPD.m
@@ -1,7 +1,7 @@
 function dPD = hmri_calc_dPD(SPD,ST1,dSPD,dST1,alpha_PD,alpha_T1,TRPD,TRT1,f_T,small_angle_approximation)
-% Calculate propagation of uncertainty for PD map
+% Calculate propagation of uncertainty for PD (or unnormalised A) map
 % (https://en.wikipedia.org/wiki/Propagation_of_uncertainty).
-% % S.Mohammadi 06.09.2019
+% S.Mohammadi 06.09.2019
 %
 % In:
 % SPD           - PDw signal at TE=0
@@ -13,19 +13,18 @@ function dPD = hmri_calc_dPD(SPD,ST1,dSPD,dST1,alpha_PD,alpha_T1,TRPD,TRT1,f_T,s
 % TRPD          - repetition time of PDw signal
 % TRT1          - repetition time of T1w signal
 % f_T           - map of transmit field
-% small_angle_approximation - Switch to turn off small-angle approximation
-% 
+% small_angle_approximation - switch to turn off small-angle approximation
 %
 % Out:
-% dPD           - error for A in [a.u.]
-% 
+% dPD           - error for PD in [a.u.]
+%
 % Reference:
-%   Mohammadi et al. NeuroImage (2022), "Error quantification in 
-%     multi-parameter mapping facilitates robust estimation and enhanced 
-%     group level sensitivity." 
+%   Mohammadi et al. NeuroImage (2022), "Error quantification in
+%     multi-parameter mapping facilitates robust estimation and enhanced
+%     group level sensitivity."
 %     https://doi.org/10.1016/j.neuroimage.2022.119529
 
-if(~isempty(f_T))
+if ~isempty(f_T)
     alpha_PD = alpha_PD.*f_T;
     alpha_T1 = alpha_T1.*f_T;
 end
@@ -42,12 +41,12 @@ dPD = sqrt( dPD_by_dS1(SPD,ST1,alpha_PD,alpha_T1,TRPD,TRT1).^2.*dSPD.^2 ...
 end
 
 function d = dPD_by_dS1(S1,S2,alpha1,alpha2,TR1,TR2)
-% Derivative of dual flip-angle A (PD) estimate with respect to first 
-% weighted signal (S1). Because of symmetry in the R1 calculation, the 
-% derivative with respect to the second weighted signal can be computed by 
+% Derivative of dual flip-angle PD (A) estimate with respect to first
+% weighted signal (S1). Because of symmetry in the PD calculation, the
+% derivative with respect to the second weighted signal can be computed by
 % permuting labels.
 %
-% Can be derived using: 
+% Can be derived using:
 %   syms S1 alpha1 S2 alpha2
 %   syms TR1 TR2 positive
 %   diff(hmri_calc_A(struct('data',S1,'fa',alpha1,'TR',TR1,'B1',1),struct('data',S2,'fa',alpha2,'TR',TR2,'B1',1),true),S1)
